1dd67ffa2eeb64bcc4e25fcbc889f8486ce49935
Mauricio650 <mauricioibanez650@gmail.com>
2025-10-25 01:29:03 -0500

    1_section finished

--- src/components/section_1/Header.tsx
+++ src/components/section_1/Header.tsx
@@ -1,20 +1,106 @@
-export function Header() {
+import { useState } from 'react'
+import { UseGS, GS } from '../../gsapConfig'
+
+interface Props {
+  handleShowMenu: () => void
+}
+
+export function Header({ handleShowMenu }: Props) {
+  const [handleMenu, setHandleMenu] = useState(false)
+  UseGS(() => {
+    GS.to('.text-gs', {
+      ease: 'power1.inOut',
+      opacity: 1,
+      y: 0,
+      duration: 0.5,
+    })
+  }, [])
+
+  const toggleMenu = () => {
+    if (handleMenu) {
+      GS.to('.stickTOP-gs', {
+        duration: 0.2,
+        ease: 'circ.inOut',
+        transformOrigin: 'center center',
+        rotation: 60,
+        y: 6,
+        width: '30px',
+        background: 'black',
+      })
+
+      GS.to('.stickBOTTOM-gs', {
+        duration: 0.2,
+        ease: 'circ.inOut',
+        transformOrigin: 'center center',
+        rotation: -60,
+        y: -6,
+        width: '30px',
+        background: 'black',
+      })
+
+      GS.to('.cart-gs ', {
+        color: 'black',
+      })
+    } else {
+      GS.to('.stickTOP-gs', {
+        duration: 0.2,
+        ease: 'circ.inOut',
+        rotation: 0,
+        background: '#F9EBCC',
+        width: '40px',
+        y: 0,
+      })
+
+      GS.to('.stickBOTTOM-gs', {
+        duration: 0.2,
+        ease: 'circ.inOut',
+        background: '#F9EBCC',
+        width: '40px',
+        rotation: 0,
+        y: 0,
+      })
+
+      GS.to('.cart-gs ', {
+        color: '#F9EBCC',
+      })
+    }
+  }
+  toggleMenu()
   return (
-    <header className="relative z-10 w-full flex justify-between items-center font-semibold lg:px-20 xl:px-35 2xl:px-50 py-12 text-lg text-[#F9EBCC]">
-      <nav>
+    <header className="relative z-12 w-full flex justify-between items-center font-semibold lg:px-20 xl:px-35 2xl:px-50 py-12 text-shadow-2xs text-shadow-black text-lg text-[#F9EBCC]">
+      <nav className="hidden xl:block ">
         <ul className="flex justify-center items-center gap-10">
-          <li className="cursor-pointer hover:text-[#fbf8f1]">Citas</li>
-          <li className="cursor-pointer hover:text-[#fbf8f1]">Tienda</li>
+          <li className="text-gs opacity-0 translate-y-3 cursor-pointer hover:text-[#fbf8f1]">
+            Citas
+          </li>
+          <li className="text-gs opacity-0 translate-y-3 cursor-pointer hover:text-[#fbf8f1]">
+            Tienda
+          </li>
         </ul>
       </nav>
+
+      <button
+        onClick={() => {
+          setHandleMenu(p => !p)
+          handleShowMenu()
+        }}
+        className="cursor-pointer text-gs translate-y-3 xl:hidden  opacity-0 flex flex-col items-center justify-center gap-[10.4px]"
+      >
+        <div className="stickTOP-gs h-0.5 w-10 bg-[#F9EBCC] "></div>
+        <div className="stickBOTTOM-gs h-0.5 w-10 bg-[#F9EBCC] "></div>
+      </button>
+
       <img
-        className="lg:w-[440px] cursor-pointer"
-        src="/logo/Looped.webp"
+        className="text-gs lg:w-[440px] cursor-pointer opacity-0 translate-y-20 filter drop-shadow-xs drop-shadow-black/50"
+        src="logo/Looped.webp"
         alt="Logo of looped. Looped is the name of hair salon"
       />
 
-      <span className="cursor-pointer hover:text-[#fbf8f1]">
+      <span className="cursor-pointer text-gs translate-y-3 hidden xl:block  opacity-0 hover:text-[#fbf8f1]">
         {'Carrito (0)'}
+      </span>
+      <span className="cart-gs cursor-pointer text-gs translate-y-3 xl:hidden  opacity-0 hover:text-[#fbf8f1]">
+        {'(0)'}
       </span>
     </header>
   )
--- src/layout/Section1.tsx
+++ src/layout/Section1.tsx
@@ -1,23 +1,66 @@
+import { useState } from 'react'
 import { Header } from '../components/section_1/Header'
+import { GS, UseGS } from '../gsapConfig'
 
 export function Section1() {
+  const [showMenu, setShowMenu] = useState(false)
   const className: string =
-    'relative bg-center bg-no-repeat bg-cover h-[68.5vh] lg:h-[99.8vh] flex flex-col justify-between items-center'
+    'relative bg-center bg-no-repeat bg-cover h-[68.5vh] lg:h-[100vh] flex flex-col justify-between items-center'
+
+  UseGS(() => {
+    GS.to('.text-gs', {
+      ease: 'power1.inOut',
+      duration: 0.5,
+      opacity: 1,
+    })
+  }, [])
+
+  UseGS(() => {
+    if (!showMenu) return
+    GS.to('.menuText-gs', {
+      ease: 'power1.inOut',
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    })
+  }, [showMenu])
+
+  const handleShowMenu = () => {
+    setShowMenu(p => !p)
+    document.body.style.overflow = 'hidden'
+  }
+
   return (
     <>
+      {/* burger menu for smaller screens */}
+      {showMenu && (
+        <section className="fixed z-11 inset-0 w-screen h-screen bg-[#A2B6A2] text-black font-semibold text-3xl">
+          <nav className="w-full h-full flex  justify-start center items-center p-20">
+            <ul className="flex justify-center items-start gap-2 flex-col">
+              <li className="menuText-gs opacity-0 translate-y-5 cursor-pointer hover:text-[#fbf8f1]">
+                Citas
+              </li>
+              <li className="menuText-gs opacity-0 translate-y-5 cursor-pointer hover:text-[#fbf8f1]">
+                Tienda
+              </li>
+            </ul>
+          </nav>
+        </section>
+      )}
+
       <section
         className={className}
-        style={{ backgroundImage: "url('/section_1/Looped-hero.webp')" }}
+        style={{ backgroundImage: "url('section_1/Looped-hero.webp')" }}
       >
         {/* opacity to highlight Header against image */}
         <div className="absolute inset-0 bg-black/10"></div>
-        <Header></Header>
+        <Header handleShowMenu={handleShowMenu}></Header>
 
-        <article className="relative z-10 flex flex-col justify-center items-center text-md font-[500] text-[#F9EBCC] pb-20">
-          <span>
+        <article className="relative z-10 flex flex-col justify-center items-center text-shadow-2xs text-shadow-black text-md font-[500] text-[#F9EBCC] pb-20">
+          <span className="text-gs opacity-0">
             Cra. 51B & Cl. 86, Riomar, Barranquilla, Atlántico, Colombia
           </span>
-          <span>loompedInfo@Outlook.com</span>
+          <span className="text-gs opacity-0">loompedInfo@Outlook.com</span>
         </article>
       </section>
     </>
